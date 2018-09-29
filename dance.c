3836629354ef69294ad8da375a29bbbf0973d30f
AbelChT <abelcht@outlook.es>
2018-09-29 11:57:01 +0200

    nps

---
+++ dance.c
@@ -0,0 +1,38 @@
+//
+// Created by abel on 29/09/18.
+//
+
+#include "dance.h"
+
+/**
+ * Function executed only first time mode is executed
+ */
+void dance_mode_create(){}
+
+/**
+ * Function executed when mode is stopped and relaunch.
+ * If on_mode_create is executed, then this will be executed
+ */
+void dance_mode_start(){}
+
+/**
+ * Function executed when mode is paused an resume ( When someone press the mode assigned key ).
+ * If on_mode_start is executed, then this will be executed
+ */
+void dance_mode_resume(){}
+
+/**
+ * Function executed when mode is paused ( When someone press the mode assigned key )
+ */
+void dance_mode_pause(){}
+
+/**
+ * Function executed when mode is stopped ( When someone press a different mode assigned key ). Its always be executed after
+ * on_mode_pause.
+ */
+void dance_mode_stop(){}
+
+/**
+ * Main loop of the mode
+ */
+void dance_mode_loop(){}
