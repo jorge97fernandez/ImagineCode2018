1421f63723ea43729317e49d0b1c6f12694ef64d
Abel Chils <abelcht@outlook.es>
2018-09-29 22:26:25 +0200

    ss

--- race_white.c
+++ race_white.c
@@ -7,18 +7,13 @@
 static unsigned int sensors[5]; // an array to hold sensor values
 static int integral = 0;
 static int last_proportional = 0;
+static const int max = 140;
 static int power_difference, derivative, proportional;
 static unsigned int position;
 
 void race_white_mode_resume(){
   set_motors(0, 0);
 }
-
-#define WEIGHT_ACTUAL 1250
-#define WEIGHT_INTEGRAL 1
-#define WEIGHT_DERIVATE 1000
-#define GLOBAL_DIVISOR 10000
-#define MAX_SPEED 140
 
 /**
  * Main loop of the mode
@@ -36,13 +31,17 @@ void race_white_mode_loop() {
         if (position >= 0 && position < 2000) proportional = 2000;
         else proportional = -2000;
     }
+    if(position < 100 || position > 3900){
+        derivative = 0;
+        integral = 0;
+    }
     derivative = proportional - last_proportional;
     integral += proportional;
     last_proportional = proportional;
-    power_difference = (int)(((long) proportional * WEIGHT_ACTUAL  + (long) integral * WEIGHT_INTEGRAL + (long) derivative * WEIGHT_DERIVATE) / 10000);
+    power_difference = proportional *1/8 + integral / 10000 + derivative * 25/10;
 
-    if (power_difference > MAX_SPEED) power_difference = MAX_SPEED;
-    if (power_difference < -MAX_SPEED) power_difference = -MAX_SPEED;
-    if (power_difference < 0) set_motors(MAX_SPEED + power_difference, MAX_SPEED);
-    else set_motors(MAX_SPEED, MAX_SPEED - power_difference);
+    if (power_difference > max) power_difference = max;
+    if (power_difference < -max) power_difference = -max;
+    if (power_difference < 0) set_motors(max + power_difference, max);
+    else set_motors(max, max - power_difference);
 }
